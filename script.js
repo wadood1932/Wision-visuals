06268888e3d1e0605ca177e3cde774eeb8a155be
wadood1932 <wadood1932@gmail.com>
2025-03-22 01:29:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -89,3 +89,11 @@ document.getElementById('appointment-form').addEventListener('submit', (e) => {
   alert('Appointment request sent! We’ll contact you shortly.');
   e.target.reset();
 });
+// Preloader Logic
+window.onload = () => {
+  const preloader = document.querySelector('.preloader');
+  setTimeout(() => {
+    preloader.style.opacity = '0';
+    setTimeout(() => preloader.remove(), 1000);
+  }, 2000); // Adjust time to match video length
+};
