5ff815aa6ed7b54c5d7c85331b2e9dee8978429e
wadood1932 <wadood1932@gmail.com>
2025-03-23 01:26:11 +0100

    Update loader.js

--- loader.js
+++ loader.js
@@ -20,3 +20,60 @@ document.querySelectorAll('.preloader-text span').forEach((letter, index) => {
     from { opacity: 0; transform: translateY(20px); }
     to { opacity: 1; transform: translateY(0); }
 }
+// QUANTUM PRELOADER SYSTEM
+class QuantumPreloader {
+  constructor() {
+    this.preloader = document.getElementById('wision-preloader');
+    this.letters = document.querySelectorAll('.preloader-text span');
+    this.init();
+  }
+
+  init() {
+    this.animateLetters();
+    this.createParticles();
+    this.bindEvents();
+  }
+
+  animateLetters() {
+    this.letters.forEach((letter, index) => {
+      gsap.from(letter, {
+        duration: 0.8,
+        opacity: 0,
+        y: 40,
+        rotationX: 90,
+        delay: index * 0.15,
+        ease: "power4.out"
+      });
+    });
+  }
+
+  createParticles() {
+    const particleCount = 30;
+    for(let i = 0; i < particleCount; i++) {
+      const particle = document.createElement('div');
+      particle.className = 'quantum-particle';
+      this.preloader.appendChild(particle);
+      
+      gsap.to(particle, {
+        x: () => gsap.utils.random(-50, 50),
+        y: () => gsap.utils.random(-50, 50),
+        rotation: 360,
+        duration: 2,
+        repeat: -1,
+        ease: "none"
+      });
+    }
+  }
+
+  bindEvents() {
+    window.addEventListener('load', () => {
+      gsap.to(this.preloader, {
+        opacity: 0,
+        duration: 1,
+        onComplete: () => this.preloader.remove()
+      });
+    });
+  }
+}
+
+new QuantumPreloader();
