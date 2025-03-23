775284ea57b1586f66eee8db5f023b839ad4a75a
wadood1932 <wadood1932@gmail.com>
2025-03-23 01:28:27 +0100

    Update physics.js

--- physics.js
+++ physics.js
@@ -18,3 +18,57 @@ class NavPhysics {
 }
 
 new NavPhysics();
+// CINEMATIC NAVIGATION PHYSICS
+class NavigationEngine {
+  constructor() {
+    this.nav = document.querySelector('.main-nav');
+    this.links = document.querySelectorAll('.nav-links a');
+    this.init();
+  }
+
+  init() {
+    this.addHoverPhysics();
+    this.addScrollEffects();
+  }
+
+  addHoverPhysics() {
+    this.links.forEach(link => {
+      link.addEventListener('mouseenter', () => {
+        gsap.to(link, {
+          y: -5,
+          scale: 1.1,
+          color: '#C5A030',
+          duration: 0.3,
+          ease: "power2.out"
+        });
+      });
+
+      link.addEventListener('mouseleave', () => {
+        gsap.to(link, {
+          y: 0,
+          scale: 1,
+          color: 'white',
+          duration: 0.4,
+          ease: "elastic.out(1, 0.5)"
+        });
+      });
+    });
+  }
+
+  addScrollEffects() {
+    let lastScroll = 0;
+    window.addEventListener('scroll', () => {
+      const currentScroll = window.scrollY;
+      
+      if(currentScroll > lastScroll) {
+        gsap.to(this.nav, { y: -100, duration: 0.3 });
+      } else {
+        gsap.to(this.nav, { y: 0, duration: 0.3 });
+      }
+      
+      lastScroll = currentScroll;
+    });
+  }
+}
+
+new NavigationEngine();
